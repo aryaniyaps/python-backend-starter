513c045ebf61e4bcd378ec125bf74f1f145630a0
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2023-12-02 12:30:37 +0530

    update constants.py

--- app/core/constants.py
+++ app/core/constants.py
@@ -1,7 +1,11 @@
+# Branding
+
 APP_NAME = "Starter"
 
 APP_URL = "https://example.com"
 
 SUPPORT_EMAIL = "support@example.com"
+
+# Password reset tokens
 
 PASSWORD_RESET_TOKEN_EXPIRES_IN = 300  # 5 minutes
