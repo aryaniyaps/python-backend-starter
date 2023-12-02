9a71d78521eb665de1c3858c322cc33d9c9e3eca
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2023-12-02 12:29:09 +0530

    avoid hardcoding token expiration time in templates

--- app/core/templates.py
+++ app/core/templates.py
@@ -1,6 +1,14 @@
+from datetime import timedelta
+
+from humanize import naturaldelta
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-from app.core.constants import APP_NAME, APP_URL, SUPPORT_EMAIL
+from app.core.constants import (
+    APP_NAME,
+    APP_URL,
+    PASSWORD_RESET_TOKEN_EXPIRES_IN,
+    SUPPORT_EMAIL,
+)
 
 
 def add_globals(environment: Environment) -> None:
@@ -26,8 +34,22 @@ environment = create_environment()
 
 reset_password_html = environment.get_template(
     name="reset_password/content.html",
+    globals={
+        "token_expires_in": naturaldelta(
+            timedelta(
+                seconds=PASSWORD_RESET_TOKEN_EXPIRES_IN,
+            ),
+        ),
+    },
 )
 
 reset_password_text = environment.get_template(
     name="reset_password/content.txt",
+    globals={
+        "token_expires_in": naturaldelta(
+            timedelta(
+                seconds=PASSWORD_RESET_TOKEN_EXPIRES_IN,
+            ),
+        ),
+    },
 )
