2ac37b34fdaeb882e9f0d759855970403a34f9ff
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2024-01-28 06:38:29 +0530

    update CORS headers

--- app/__init__.py
+++ app/__init__.py
@@ -44,8 +44,8 @@ def add_middleware(app: FastAPI) -> None:
         CORSMiddleware,
         allow_origins=settings.cors_allow_origins,
         allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+        allow_headers=["X-Requested-With", "X-Request-ID"],
+        expose_headers=["X-Request-ID"],
     )
     app.add_middleware(
         BaseHTTPMiddleware,
