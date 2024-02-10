d68648c6d63589c14a03e9489b0ee80f6b21605d
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2024-02-10 10:09:11 +0530

    add prefix to health router

--- app/health/routes.py
+++ app/health/routes.py
@@ -4,12 +4,13 @@ from app.core.constants import OpenAPITag
 from app.health.schemas import HealthCheckResult
 
 health_router = APIRouter(
+    prefix="/health",
     tags=[OpenAPITag.HEALTH],
 )
 
 
 @health_router.get(
-    "/health",
+    "/",
     response_model=HealthCheckResult,
     summary="Check the health status of the application.",
     description="Provides information about the health status of the application.",
