7a71a7dd3ffe984b4ccca3dbde643768ed9028c0
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2024-02-10 17:36:31 +0530

    reset migrations

--- migrations/versions/2024_02_10_create_extensions.py
+++ migrations/versions/2024_02_10_create_extensions.py
@@ -1,18 +1,19 @@
 """
 create extensions
 
-Revision ID: e404ef6a54b5
+Revision ID: 7a23de63905c
 Revises:
-Create Date: 2024-01-23 09:42:17.516135
+Create Date: 2024-02-10 17:35:25.715775
 
 """
+
 from collections.abc import Sequence
 
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "e404ef6a54b5"
+revision: str = "7a23de63905c"
 down_revision: str | None = None
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
--- migrations/versions/2024_02_10_initial_migration.py
+++ migrations/versions/2024_02_10_initial_migration.py
@@ -1,11 +1,12 @@
 """
 initial migration
 
-Revision ID: 98fa494ae1a0
-Revises: e404ef6a54b5
-Create Date: 2024-01-23 12:33:22.628033
+Revision ID: 6a6f953b7c2f
+Revises: 7a23de63905c
+Create Date: 2024-02-10 17:36:07.254921
 
 """
+
 from collections.abc import Sequence
 
 import sqlalchemy as sa
@@ -13,8 +14,8 @@ from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision: str = "98fa494ae1a0"
-down_revision: str | None = "e404ef6a54b5"
+revision: str = "6a6f953b7c2f"
+down_revision: str | None = "7a23de63905c"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
@@ -40,36 +41,6 @@ def upgrade() -> None:
     op.create_index(op.f("users_email_idx"), "users", ["email"], unique=True)
     op.create_index(op.f("users_username_idx"), "users", ["username"], unique=True)
     op.create_table(
-        "login_sessions",
-        sa.Column(
-            "id", sa.Uuid(), server_default=sa.text("gen_random_uuid()"), nullable=False
-        ),
-        sa.Column("user_id", sa.Uuid(), nullable=False),
-        sa.Column("ip_address", sa.String(length=40), nullable=False),
-        sa.Column(
-            "created_at",
-            sa.DateTime(timezone=True),
-            server_default=sa.text("now()"),
-            nullable=False,
-        ),
-        sa.ForeignKeyConstraint(
-            ["user_id"], ["users.id"], name=op.f("login_sessions_user_id_fkey")
-        ),
-        sa.PrimaryKeyConstraint("id", name=op.f("login_sessions_pkey")),
-        sa.UniqueConstraint(
-            "user_id", "ip_address", name=op.f("login_sessions_user_id_key")
-        ),
-    )
-    op.create_index(
-        op.f("login_sessions_user_id_idx"), "login_sessions", ["user_id"], unique=False
-    )
-    op.create_index(
-        "login_sessions_user_id_ip_address_idx",
-        "login_sessions",
-        ["user_id", "ip_address"],
-        unique=True,
-    )
-    op.create_table(
         "password_reset_tokens",
         sa.Column(
             "id", sa.Uuid(), server_default=sa.text("gen_random_uuid()"), nullable=False
@@ -94,16 +65,46 @@ def upgrade() -> None:
         ["token_hash"],
         unique=True,
     )
+    op.create_table(
+        "user_sessions",
+        sa.Column(
+            "id", sa.Uuid(), server_default=sa.text("gen_random_uuid()"), nullable=False
+        ),
+        sa.Column("user_id", sa.Uuid(), nullable=False),
+        sa.Column("ip_address", sa.String(length=40), nullable=False),
+        sa.Column("location", sa.String(length=256), nullable=False),
+        sa.Column("user_agent", sa.String(length=256), nullable=False),
+        sa.Column("logged_out_at", sa.DateTime(timezone=True), nullable=True),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"], ["users.id"], name=op.f("user_sessions_user_id_fkey")
+        ),
+        sa.PrimaryKeyConstraint("id", name=op.f("user_sessions_pkey")),
+    )
+    op.create_index(
+        op.f("user_sessions_user_id_idx"), "user_sessions", ["user_id"], unique=False
+    )
+    op.create_index(
+        "user_sessions_user_id_ip_address_idx",
+        "user_sessions",
+        ["user_id", "ip_address"],
+        unique=False,
+    )
 
 
 def downgrade() -> None:
+    op.drop_index("user_sessions_user_id_ip_address_idx", table_name="user_sessions")
+    op.drop_index(op.f("user_sessions_user_id_idx"), table_name="user_sessions")
+    op.drop_table("user_sessions")
     op.drop_index(
         op.f("password_reset_tokens_token_hash_idx"), table_name="password_reset_tokens"
     )
     op.drop_table("password_reset_tokens")
-    op.drop_index("login_sessions_user_id_ip_address_idx", table_name="login_sessions")
-    op.drop_index(op.f("login_sessions_user_id_idx"), table_name="login_sessions")
-    op.drop_table("login_sessions")
     op.drop_index(op.f("users_username_idx"), table_name="users")
     op.drop_index(op.f("users_email_idx"), table_name="users")
     op.drop_table("users")
