472e0e8df415c03951e9e96ba96b3139f9a2373e
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2024-02-29 12:16:37 +0530

    add react-hook-form

--- client/app/(authentication)/login/page.tsx
+++ client/app/(authentication)/login/page.tsx
@@ -1,5 +1,7 @@
+'use client';
 import { APP_NAME } from '@/lib/constants';
 import {
+  Button,
   Card,
   CardBody,
   CardFooter,
@@ -7,15 +9,28 @@ import {
   Input,
   Link,
 } from '@nextui-org/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type LoginInput = {
+  email: string;
+};
 
 export default function LoginPage() {
+  const { register, handleSubmit } = useForm<LoginInput>({});
+
+  const onSubmit: SubmitHandler<LoginInput> = (data) => console.log(data);
   return (
     <Card isFooterBlurred fullWidth className='px-unit-2'>
       <CardHeader>
         <h1 className='text-md font-semibold'>Sign in with {APP_NAME}</h1>
       </CardHeader>
       <CardBody>
-        <Input type='email' label='Email address' />
+        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4'>
+          <Input type='email' label='Email address' {...register('email')} />
+          <Button color='primary' type='submit'>
+            login with passkeys
+          </Button>
+        </form>
       </CardBody>
       <CardFooter className='text-sm'>
         Don&apos;t have an account?&nbsp;<Link href='/register'>sign up</Link>
--- client/app/(authentication)/register/page.tsx
+++ client/app/(authentication)/register/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { APP_NAME } from '@/lib/constants';
 import {
   Button,
@@ -8,16 +9,35 @@ import {
   Input,
   Link,
 } from '@nextui-org/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type RegisterInput = {
+  email: string;
+  displayName: string;
+};
+
 export default function RegisterPage() {
+  const { register, handleSubmit } = useForm<RegisterInput>({});
+
+  const onSubmit: SubmitHandler<RegisterInput> = (data) => console.log(data);
+
   return (
     <Card isFooterBlurred fullWidth className='px-unit-2'>
       <CardHeader>
         <h1 className='text-md font-semibold'>Create a {APP_NAME} account</h1>
       </CardHeader>
-      <CardBody className='flex flex-col gap-4'>
-        <Input type='email' label='Email address' />
-        <Input type='text' label='Display name' />
-        <Button color='primary'>Continue</Button>
+      <CardBody>
+        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4'>
+          <Input type='email' label='Email address' {...register('email')} />
+          <Input
+            type='text'
+            label='Display name'
+            {...register('displayName')}
+          />
+          <Button color='primary' type='submit'>
+            Continue
+          </Button>
+        </form>
       </CardBody>
       <CardFooter className='text-sm'>
         Have an account?&nbsp;<Link href='/login'>sign in</Link>
